37732f8fdf3604aa0de296f82703cab3f8418c23
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-02-19 19:33:01 +0300

    Change import from content.js to fetch from json

--- js/createTable.js
+++ js/createTable.js
@@ -1,21 +1,34 @@
-import table from "./content.js";
+(async () => {
+    const container = document.querySelector(".container");
+    const columnSequence = ["title", "icon", "name", "descr"];
+    const iconsClass = "material-icons";
+    let table;
 
-const container = document.querySelector(".container");
-const columnSequence = ["title", "icon", "name", "descr"];
-const iconsClass = "material-icons";
-
-table.forEach(element => {
-
-    columnSequence.forEach(field => {
-        if (!element[field]) return;
-
-        const div = document.createElement("div");
-        div.textContent = element[field];
-        container.appendChild(div);
-
-        div.classList.add("cell", field);
-        if (field === "icon") {
-            div.classList.add(iconsClass);
+    try {
+        const response = await fetch("../content.json");
+        if (response.ok) {
+            table = await response.json();
+        } else {
+            container.textContent = "Не удалось загрузить данные";
+            throw new Error(response.status);
         }
+    } catch (error) {
+        throw error;
+    }
+
+    table.forEach(element => {
+
+        columnSequence.forEach(field => {
+            if (!element[field]) return;
+
+            const div = document.createElement("div");
+            div.textContent = element[field];
+            container.appendChild(div);
+
+            div.classList.add("cell", field);
+            if (field === "icon") {
+                div.classList.add(iconsClass);
+            }
+        });
     });
-});
+})();
